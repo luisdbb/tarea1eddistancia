e5b25faa0e058408455aa94f85f4ca73f01bbaba
pablold <pablold@PAVANE_HP>
2022-11-05 13:01:21 +0100

    primer commit

--- Tarea1EDdistancia/src/estudiantes/MainTXJ21094.java
+++ Tarea1EDdistancia/src/estudiantes/MainTXJ21094.java
@@ -3,7 +3,7 @@ package estudiantes;
 public class MainTXJ21094 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola! Soy Pablo López Díaz (TXJ21094@educastur.es)");
 
 	}
 
