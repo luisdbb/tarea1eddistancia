6fe03ea4f66fdb7608b5d88b9dbfcd841bff6070
TXJ21094 <117480305+TXJ21094@users.noreply.github.com>
2022-11-14 21:12:14 +0100

    Update MainTXJ21094.java

--- Tarea1EDdistancia/src/estudiantes/MainTXJ21094.java
+++ Tarea1EDdistancia/src/estudiantes/MainTXJ21094.java
@@ -3,7 +3,7 @@ package estudiantes;
 public class MainTXJ21094 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola! Soy Pablo López Díaz (TXJ21094@educastur.es)");
+		System.out.println("Hola!  Soy Pablo López Díaz (TXJ21094@educastur.es)");
 
 	}
 
