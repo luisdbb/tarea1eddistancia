63c305905dd258616e426fccd102c5766321efc6
dgarcia <dgarcia@dgv.home>
2022-11-17 18:14:00 +0100

    subida de ejercicio 6

---
+++ Tarea1EDdistancia/src/estudiantes/MainABK44559.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainABK44559 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy David Garcia Vincelle (ABK44559@educastur.es)");
+	}
+
+}
