666025e1d9d8690bc2228ced30fc68c9709a30fd
JPabloSF <evx77052@educastur.es>
2022-11-20 21:01:24 +0100

    Añadido fichero MainEVX77052.java

---
+++ Tarea1EDdistancia/src/estudiantes/MainEVX77052.java
@@ -0,0 +1,12 @@
+package estudiantes;
+
+public class MainEVX77052 {
+	public static void main(String[] args) {
+		System.out.println("Nombre   : Juan Pablo");
+		System.out.println("Apellidos: Sierra Fernández");
+		System.out.println("Correo   : evx77052@educastur.es");
+	}
+	
+	
+
+}
