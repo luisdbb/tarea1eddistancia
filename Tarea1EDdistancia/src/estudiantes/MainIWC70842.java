f1317ee88a7d747500eb0a8c6c71b34cd3e04dac
IWC70842 <iwc70842@educastur.es>
2022-11-21 18:49:32 +0100

    Entrega apartado 6 de la tarea IWC70842

---
+++ Tarea1EDdistancia/src/estudiantes/MainIWC70842.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class MainIWC70842 {
+
+	public static void main(String[] args) {
+		// Mis datos para el apartado 6 de la tarea 1 de Endes
+		System.out.println("Hola!\nSoy Jose Antonio Pozo Gonzalez (IWC70842@educastur.es)");
+	}
+
+}
