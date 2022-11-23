99bd401865aceb0a2be7591bf478d1c27821d704
mobyd <mobyd@DESKTOP-RRFPL92>
2022-11-23 19:06:03 +0100

    subido el fichero .java

---
+++ Tarea1EDdistancia/src/estudiantes/MainDNT78196.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class MainDNT78196 {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Soy Jesús Mª Braña Rodríguez-Abello, mi email: DNT78196@educastur.es");
+	}
+
+}
