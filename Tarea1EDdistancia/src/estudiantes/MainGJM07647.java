61534ac649472402e74d1017a0c3d9aaaa874e83
Guty <Guty@GUTYPC>
2022-11-23 22:57:34 +0100

    Tarea 1 actualizada

---
+++ Tarea1EDdistancia/src/estudiantes/MainGJM07647.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainGJM07647 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Ivan Gutierrez Raimundo (GJM07647@educastur.es)");
+	}
+
+}
