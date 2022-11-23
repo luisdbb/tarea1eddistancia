4ed62747ff71dfd313937533fb6838ae97d5be1a
GLJ20163 <glj20163@educastur.es>
2022-11-23 18:03:50 +0100

    Actualización con otro fichero más :)

---
+++ Tarea1EDdistancia/src/estudiantes/MainGLJ20163.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainGLJ20163 {
+	public static void main(String[] args) {
+		System.out.println ("Hola, mis datos son los siguientes:");
+		System.out.println ("Nombre: IVAN ESCUDERO MARTIN");
+		System.out.println ("Email: glj20163@educastur.es");
+	}
+}
