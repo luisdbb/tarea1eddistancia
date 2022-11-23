b2550cd47dc194807926f7f2c961f66ac6b29d10
luisdbb <92033626+luisdbb@users.noreply.github.com>
2022-11-23 18:10:10 +0100

    Update MainXXX.java

--- Tarea1EDdistancia/src/estudiantes/MainXXX.java
+++ Tarea1EDdistancia/src/estudiantes/MainXXX.java
@@ -4,6 +4,15 @@ public class MainXXX {
 
 	public static void main(String[] args) {
 		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
+		System.out.println("Hola! Soy Luis de Blas, el profesor (luisdbb@educastur.org)");
 
 	}
 
