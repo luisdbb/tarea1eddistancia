c70c56d81126b2520aeccafaeff7dbbe9f9d3456
Paula <developau7@gmail.com>
2022-11-26 14:19:12 +0100

    primer commit

---
+++ Tarea1EDdistancia/src/estudiantes/MainQSZ54245.java
@@ -0,0 +1,11 @@
+package estudiantes;
+
+public class MainQSZ54245 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Paula Pérez Zapico (QSZ54245@educastur.org)");
+
+		
+	}
+
+}
