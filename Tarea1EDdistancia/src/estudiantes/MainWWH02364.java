87a262a440e038f13683f6ae880cb6fad358e47d
WWH02364 <WWH02364@educastur.es>
2022-11-26 10:23:48 +0100

    Subida de tarea 6

---
+++ Tarea1EDdistancia/src/estudiantes/MainWWH02364.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class MainWWH02364 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Rubén Galdón (WWH02364@educastur.org)");
+
+	}
+
+}
