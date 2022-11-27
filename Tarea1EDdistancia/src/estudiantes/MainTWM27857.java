2b86160893e6047814b431a6cb1179dc8d646c5a
TWM27857 <TWM27857@educastur.es>
2022-11-27 19:49:22 +0100

    Subir fichero ejercicio 6

---
+++ Tarea1EDdistancia/src/estudiantes/MainTWM27857.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainTWM27857 {
+
+	public static void main(String[] args) {
+		System.out.println("¡Hola! Soy Paula Toral y mi email es: TWM27857@educastur.es");
+	}
+
+}
