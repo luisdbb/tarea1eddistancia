634dfa523a0e530be711c4e62258ee76b4117c8f
jalbertolv16 <118838141+jalbertolv16@users.noreply.github.com>
2022-11-27 15:49:13 +0100

    Agregando Mainjalbertolv16

---
+++ Mainjalbertolv16.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class Mainjalbertolv16 {
+
+	public static void main(String[] args) {
+		System.out.println("¡Hola! Soy Jesús Alberto López Vázquez, mi correo es jalbertolv16@educastur.org");
+		
+	}
+
+}
