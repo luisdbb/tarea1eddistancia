d7d570287134d0cd90a142c9c8d805069b070b68
PGValle <JLA02430@educastur.es>
2022-11-28 23:31:38 +0100

    Se sube el archivo class

---
+++ Tarea1EDdistancia/src/estudiantes/MainJLA02430.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainJLA02430 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Pablo González Valle, el alumno (JLA02430@educastur.es)");
+	}
+
+}
