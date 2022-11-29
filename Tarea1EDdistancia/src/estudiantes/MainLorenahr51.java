1822229f3788031a511df34c12f49c6a51f33df6
lilyan <lilyan@DESKTOP-7BHQTBA>
2022-11-29 20:37:21 +0100

    Commit Tarea 1 Entornos

---
+++ Tarea1EDdistancia/src/estudiantes/MainLorenahr51.java
@@ -0,0 +1,13 @@
+package estudiantes;
+
+public class MainLorenahr51 {
+
+	public static void main(String[] args) {
+		System.out.println(""
+				+ "Hola, soy Lorena Hernández del Reguero "
+				+ "Email Educastur: lorenahr51@educastur.es");
+	}
+
+}
+
+
