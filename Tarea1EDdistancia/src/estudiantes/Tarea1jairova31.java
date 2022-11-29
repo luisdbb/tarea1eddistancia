19314ad0fc6fa61d71004c6d9f1a1cc753e13084
jairova31 <117950649+Jairobm@users.noreply.github.com>
2022-11-29 21:39:51 +0100

    Add files via upload

---
+++ Tarea1EDdistancia/src/estudiantes/Tarea1jairova31.java
@@ -0,0 +1,15 @@
+package estudiantes;
+
+	
+
+	public class Mainjairova31{
+		public static void main(String[] args) {
+		
+		System.out.println("Hola " + "mi nombre y mis apellidos son:");
+		System.out.println("Jairo Villanueva Alvarez");
+		System.out.println("Correo: jairova31@educastur.es");
+
+		
+	}
+
+}
