4a2dcacacbd8d6980e385ace885efe2029856fbf
NuriaMQ <GIT64044@educastur.es>
2022-11-29 16:33:34 +0100

    añado el fichero de Nuria

---
+++ Tarea1EDdistancia/src/estudiantes/MainGIT64044.java
@@ -0,0 +1,10 @@
+package estudiantes;
+
+public class MainGIT64044 {
+
+	public static void main(String[] args) {
+		System.out.println("¡Hola! Soy Nuria Martinez Quiros");
+		System.out.println("mi email es: GIT64044@educastur.es");
+	}
+
+}
