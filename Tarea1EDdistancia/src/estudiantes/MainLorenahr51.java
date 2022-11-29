a280726069f51c634a1cc1e41206f25218cba75c
lilyan <lilyan@DESKTOP-7BHQTBA>
2022-11-29 20:57:28 +0100

    2º Commit con Cambios Tarea 1

--- Tarea1EDdistancia/src/estudiantes/MainLorenahr51.java
+++ Tarea1EDdistancia/src/estudiantes/MainLorenahr51.java
@@ -3,9 +3,7 @@ package estudiantes;
 public class MainLorenahr51 {
 
 	public static void main(String[] args) {
-		System.out.println(""
-				+ "Hola, soy Lorena Hernández del Reguero "
-				+ "Email Educastur: lorenahr51@educastur.es");
+		System.out.println("Hola, soy Lorena Hernández del Reguero Email Educastur: lorenahr51@educastur.es ");
 	}
 
 }
