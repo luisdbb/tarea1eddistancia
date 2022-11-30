dd39216ae1216b3d7caf01e0d8f82153ea6b96b8
Jose <Jose@192.168.1.35>
2022-11-30 01:13:13 +0100

    Ejercicio 6

---
+++ Tarea1EDdistancia/src/estudiantes/MainNUS30084.java
@@ -0,0 +1,9 @@
+package estudiantes;
+
+public class MainNUS30084 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola! Soy Jose Ramon Remis Chacon (NUS30084@educastur.es)");
+	}
+
+}
